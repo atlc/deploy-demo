af9047bf759ed809e0ec284bce8df4df3c728f31
MD <mdeanwill@gmail.com>
2024-03-08 15:39:12 -0800

    fixed mentions table

--- src/server/db/tables/mentionstable.ts
+++ src/server/db/tables/mentionstable.ts
@@ -1,4 +1,4 @@
-import { SelectQuery } from "../queryUtils";
+import { SelectQuery, ModifyQuery } from "../queryUtils";
 import type { RowDataPacket } from "mysql2";
 
 export interface IMentionsRow extends RowDataPacket {
@@ -8,4 +8,8 @@ export interface IMentionsRow extends RowDataPacket {
 
 export function getALLMentions() {
     return SelectQuery<IMentionsRow>('SELECT * FROM mentions;')
+}
+
+export function insertMention(chirp_id:number, user_id:number) {
+    return ModifyQuery('INSERT INTO mentions (chirp_id, user_id) VALUE (?);', [chirp_id, user_id])
 }
