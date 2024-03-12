75555e22ddbf04efe922f480bad4c66a1d5d5e2e
MD <mdeanwill@gmail.com>
2024-03-12 15:34:54 -0700

    started navbar and views

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,23 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { fetchData } from './services/fetchData';
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+
 
 interface AppProps {}
 
 const App = (props: AppProps) => {
-	const [data, setData] = useState('');
-
-	useEffect(() => {
-		fetchData('/api/chirps')
-			.then(res => res.json())
-			.then(data => setData(data.message))
-			.catch(e => console.log('[fetch erorr]', e));
-	}, []);
-
+	
 	return (
-		<div className="mx-auto mt-5 w-25">
-			
-		</div>
+		<BrowserRouter>
+		<Navbar bg="dark" expand="lg">
+                    <Navbar.Brand href="/">Chirper</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/chrip">Chirp It!</Nav.Link>
+                            <Nav.Link as={NavLink} to="/edit">Edit Chrips</Nav.Link>
+                            <Nav.Link as={NavLink} to="/mentions">User Mentions</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+            </Navbar>
+		<Routes>
+			<Route path="/" element={<Home />} />
+            <Route path="/chirp/:id" element={<ChirpDetails />} />
+            <Route path="/compose" element={<ChirpIt />} />
+            <Route path="/admin" element={<EditChirp />} />
+            <Route path="/mentions" element={<UserMentions />} />
+		</Routes>
+		</BrowserRouter>
 	);
 };
 
 export default App;
+
+{/* <div className="bg-dark">
+        <Link to='/' className="btn btn-light m-3">Home</Link>
+        <Link to='/films' className="btn btn-light m-3">View Chirps</Link>
+        <Link to='/people' className="btn btn-light m-3">Chirp It!</Link>
+        <Link to='/people' className="btn btn-light m-3">Edit Chirps</Link>
+        <Link to='/people' className="btn btn-light m-3">User Mentions</Link>
+        </div> */}
---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HomeProps {}
+
+const Home = (props: HomeProps) => {
+  return (
+    <main className="container mt-5">
+        <section className="row justify-content-center"></section>
+    </main>
+  );
+};
+
+export default Home;
