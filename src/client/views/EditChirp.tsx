d9cfc9f68d265b07ff3de1557ed102e654ab6d76
MD <mdeanwill@gmail.com>
2024-03-18 14:29:48 -0700

    Edit Chirp complete

--- src/client/views/EditChirp.tsx
+++ src/client/views/EditChirp.tsx
@@ -3,15 +3,43 @@ import { fetchData } from '../services/fetchData';
 import { Card, Container } from 'react-bootstrap';
 import { ChirpJOIN } from '../types';
 
+
 interface EditChirpProps {}
 
 const EditChirp = (props: EditChirpProps) => {
   const [chirps, setChirps] = useState<ChirpJOIN[]>([])
+  
+  
+  const [selectedChirp, setSelectedChirp] = useState<ChirpJOIN | null> (null)
 
-  useEffect(() => {
+  function setupEdit (chirp:ChirpJOIN) {
+    setSelectedChirp(chirp)
+
+  }
+
+  function handleSubmitEdit () {
+    fetchData(`/api/chirps/${selectedChirp?.id}`, 'PUT', selectedChirp)
+      .then(() => {
+        getChirps()
+        setSelectedChirp(null)
+      })
+  }
+
+ function getChirps() {
     fetchData('/api/chirps')
       .then(chirps => setChirps(chirps))
-  })
+  }
+
+  useEffect(() => {
+    getChirps()
+  }, [])
+
+  const handleDelete = (id:number) =>{
+    
+    fetchData(`/api/chirps/${id}`, 'DELETE')
+      .then(data => console.log(data.message))
+      .then(getChirps)
+  }
 
   return (
     <Container>
@@ -24,12 +52,14 @@ const EditChirp = (props: EditChirpProps) => {
               </Card.Title>
               <Card.Body>
                 <Card.Text >
-                  {chirp.body}
+                <textarea className='form-control bg-light' value={chirp.id === selectedChirp?.id ? selectedChirp?.body : chirp.body} onChange={(e) => chirp.id === selectedChirp?.id ? setSelectedChirp({ ...selectedChirp, body: e.target.value  }) : null} readOnly={chirp.id !== selectedChirp?.id} />
+                {chirp.id === selectedChirp?.id ? <button onClick={handleSubmitEdit} className='bg-primary'>Submit</button> : <button onClick={() => setupEdit(chirp)}>Edit Body</button>}
                 </Card.Text>
               </Card.Body>
               <Card.Footer className='bg-info'>
                 Location: {chirp.location}
               </Card.Footer>
+              <button onClick={() => handleDelete(chirp.id)} className='bg-danger'>Delete Chirp</button>
             </Card>
           ))};
         </div>
