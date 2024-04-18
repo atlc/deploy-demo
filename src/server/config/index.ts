61df199fd7becd1ff4b458ef1d4e2d8d3e429612
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-04-18 13:35:33 -0500

    Fixing envar config

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -5,8 +5,9 @@ dotenv.config();
 const mysql = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    host: process.env.DB_USER,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    port: Number(process.env.PORT || 3306),
 };
 
 export default {
