486e7bee4f2435848b9fc5b4ed7fa6cc61049d18
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-04-18 13:44:12 -0500

    Switching from execute to query to allow for connection releasing

--- src/server/db/queryUtils.ts
+++ src/server/db/queryUtils.ts
@@ -1,8 +1,8 @@
-import pool from './connection';
-import type { ResultSetHeader } from 'mysql2';
+import pool from "./connection";
+import type { ResultSetHeader } from "mysql2";
 
 export async function SelectQuery<T>(queryString: string, params?: any): Promise<Partial<T>[]> {
-    const [results] = await pool.execute(queryString, params);
+    const [results] = await pool.query(queryString, params);
     return results as T[];
 }
 
